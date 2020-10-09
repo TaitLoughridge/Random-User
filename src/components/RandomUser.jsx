b90960c0fe067435d54a45ab80149ebeebff3b73
Tait Loughridge <68408313+TaitLoughridge@users.noreply.github.com>
2020-10-09 13:29:16 -0500

    start

---
+++ src/components/RandomUser.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './RandomUser.css'
+
+function RandomUser(props) {
+    const {userData } = props;
+        return (
+            <ul>
+                {userData.map(user => (
+                    <li className='user-card' key={user.login.uuid}>
+                    <img 
+                        src={user.picture.large} 
+                        alt={`${user.name.first} ${user.name.last}`}/>
+                    <p>
+                        {user.name.first} {user.name.last}
+                    </p>
+                </li>
+                ))}
+            </ul>
+        );
+};
+
+export default RandomUser;
